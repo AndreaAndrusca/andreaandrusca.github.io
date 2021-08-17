6c5d1209a230853cad114d3716683d52097ec7a9
AndreaAndrusca <88724336+AndreaAndrusca@users.noreply.github.com>
2021-08-17 18:32:26 +0200

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,6 @@ app.set("view engine", "hbs");
 app.use("/", require("./routes/pages"));
 app.use("/totalcost", require("./routes/pages"));
 
-app.listen(5000, ()=>{
+app.listen(process.env.PORT || 5000, ()=>{
     console.log("Server running on 5000");
 })
